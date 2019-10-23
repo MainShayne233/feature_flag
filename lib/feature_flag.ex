a88e703d0d624f73c7469962b9e50057debab4d3
MainShayne233 <shaynetremblay@gmail.com>
2019-10-23 18:59:05 -0400

    Implement core idea

--- lib/feature_flag.ex
+++ lib/feature_flag.ex
@@ -1,18 +1,36 @@
 defmodule FeatureFlag do
-  @moduledoc """
-  Documentation for FeatureFlag.
-  """
+  defmacro __using__(_) do
+    quote do
+      import FeatureFlag, only: [def: 3]
+    end
+  end
 
-  @doc """
-  Hello world.
+  defmacro def({func_name, _, params} = call, {:feature_flag, _, _}, expr) do
+    name = {__CALLER__.module, func_name, length(params)}
 
-  ## Examples
+    quote do
+      def unquote(call) do
+        case FeatureFlag.get(unquote(Macro.escape(name))) do
+          unquote(case_block(expr))
+        end
+      end
+    end
+  end
 
-      iex> FeatureFlag.hello()
-      :world
+  defp case_block(do: [{:->, _, _} | _] = case_block), do: case_block
 
-  """
-  def hello do
-    :world
+  defp case_block(do: do_block, else: else_block) do
+    quote do
+      true -> unquote(do_block)
+      false -> unquote(else_block)
+    end
+  end
+
+  def get(name) do
+    Application.get_env(__MODULE__, name)
+  end
+
+  def set(name, value) do
+    Application.put_env(__MODULE__, name, value)
   end
 end
--- test/feature_flag_test.exs
+++ test/feature_flag_test.exs
@@ -1,8 +1,44 @@
 defmodule FeatureFlagTest do
   use ExUnit.Case
-  doctest FeatureFlag
 
-  test "greets the world" do
-    assert FeatureFlag.hello() == :world
+  describe "def/3" do
+    test "should allow for a mutli-case definition" do
+      defmodule MyApp.A do
+        use FeatureFlag
+
+        def math(value), feature_flag do
+          :double -> 2 * value
+          :half -> value / 2
+          :mod_5 -> rem(value, 5)
+        end
+      end
+
+      FeatureFlag.set({MyApp.A, :math, 1}, :double)
+      assert MyApp.A.math(2) == 4
+
+      FeatureFlag.set({MyApp.A, :math, 1}, :half)
+      assert MyApp.A.math(4) == 2
+
+      FeatureFlag.set({MyApp.A, :math, 1}, :mod_5)
+      assert MyApp.A.math(8) == 3
+    end
+
+    test "should allow for a do/else definition" do
+      defmodule MyApp.B do
+        use FeatureFlag
+
+        def maybe_reverse_string(value), feature_flag do
+          String.reverse(value)
+        else
+          value
+        end
+      end
+
+      FeatureFlag.set({MyApp.B, :maybe_reverse_string, 1}, true)
+      assert MyApp.B.maybe_reverse_string("hello") == "olleh"
+
+      FeatureFlag.set({MyApp.B, :maybe_reverse_string, 1}, false)
+      assert MyApp.B.maybe_reverse_string("hello") == "hello"
+    end
   end
 end
